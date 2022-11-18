a709f7d73876a9077dc2b83b5c9459ba6e81ff5c
Kacper <kacper@ksiez@gmail.com>
2022-11-18 13:59:07 +0100

    TEST3

--- PHP/test.php
+++ PHP/test.php
@@ -20,7 +20,9 @@
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-        
+
+<!-- ZMIENIAM TEN PLIK TEST3 -->
+
         <form action="script.php" method="post">
             <input type="text">
             <input type="submit">
