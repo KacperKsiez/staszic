1cfbf43c693f7253bc79845a2766a11b8abba58f
Kacper <kacper@ksiez@gmail.com>
2022-11-18 13:58:21 +0100

    test2

--- Bazy danych/baza.sql
+++ Bazy danych/baza.sql
@@ -5,12 +5,22 @@ CREATE TABLE Klasy
 (
     IDKlasy int PRIMARY KEY AUTO_INCREMENT,
     Profil varchar(50)
-)
+);
 
 CREATE TABLE Przedmioty
 (
     IDPrzedmiotu int PRIMARY KEY AUTO_INCREMENT,
     Nazwa varchar(250)
-)
+);
 
 CREATE TABLE Uczniowie
+(
+    NumerLegitymacji int AUTO_INCREMENT PRIMARY KEY,
+    Imie varchar(20),
+    Nazwisko varchar(100),
+    IDKlasy int
+);
+
+ALTER TABLE Uczniowie
+ADD FOREIGN KEY (IDKlasy)
+REFERENCES Klasy(IDKlasy);
